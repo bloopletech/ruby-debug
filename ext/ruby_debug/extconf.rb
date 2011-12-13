bf19f821ab576077cd38126db945a2b20a5571bd
Brenton Fletcher <brenton@coupa.com>
2011-12-13 17:03:59 +1100

    Make it work under 1.9.3

--- ext/ruby_debug/extconf.rb
+++ ext/ruby_debug/extconf.rb
@@ -6,17 +6,16 @@ if RUBY_VERSION < "1.9"
   exit(1)
 end
 
-hdrs = proc {
+hdrs = lambda {
   iseqs = %w[vm_core.h iseq.h]
-  begin
+  return false unless begin
     have_struct_member("rb_method_entry_t", "called_id", "method.h") or
     have_struct_member("rb_control_frame_t", "method_id", "method.h")
-  end and
-  have_header("vm_core.h") and have_header("iseq.h") and have_header("insns.inc") and
-  have_header("insns_info.inc") and have_header("eval_intern.h") or break
-  have_type("struct iseq_line_info_entry", iseqs) or
-  have_type("struct iseq_insn_info_entry", iseqs) or
-  break
+  end &&
+  have_header("vm_core.h") && have_header("iseq.h") && have_header("insns.inc") &&
+  have_header("insns_info.inc") && have_header("eval_intern.h")
+  return false unless have_type("struct iseq_line_info_entry", iseqs) ||
+  have_type("struct iseq_insn_info_entry", iseqs)
   if checking_for(checking_message("if rb_iseq_compile_with_option was added an argument filepath")) do
       try_compile(<<SRC)
 #include <ruby.h>
